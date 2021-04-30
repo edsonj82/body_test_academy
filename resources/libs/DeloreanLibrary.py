00b753a6f3e48679157c3a494893c521b0cfa031
Edson <edsonj82@outlook.com>
2021-04-30 13:38:23 -0300

    Refactoring DeloreanLibrary (connect method)

--- resources/libs/DeloreanLibrary.py
+++ resources/libs/DeloreanLibrary.py
@@ -2,6 +2,14 @@ import psycopg2
 from logging import info
 
 class DeloreanLibrary():
+
+    def connect(self):
+        return psycopg2.connect(
+            host='ec2-3-233-43-103.compute-1.amazonaws.com',
+            database='d1n6rrmc0ou8vl',
+            user='fyxxegvbexbhlu',
+            password='f57ada515fe4939c7bba61bd0567805aff2390419315d79140b1ac88dd2441aa'
+        )
     
     #No Robot vira uma KW automaticamente => Remove Student     email.com@thotokus.com
     def remove_student(self, email):
@@ -9,12 +17,7 @@ class DeloreanLibrary():
         query = "delete from students where email = '{}'".format(email)
         info(query)
         
-        conn = psycopg2.connect(
-            host='ec2-3-233-43-103.compute-1.amazonaws.com',
-            database='d1n6rrmc0ou8vl',
-            user='fyxxegvbexbhlu',
-            password='f57ada515fe4939c7bba61bd0567805aff2390419315d79140b1ac88dd2441aa'
-        )
+        conn = self.connect()
         
         cur = conn.cursor()
         cur.execute(query)
@@ -30,12 +33,8 @@ class DeloreanLibrary():
         )
         info(query)
 
-        conn = psycopg2.connect(
-            host='ec2-3-233-43-103.compute-1.amazonaws.com',
-            database='d1n6rrmc0ou8vl',
-            user='fyxxegvbexbhlu',
-            password='f57ada515fe4939c7bba61bd0567805aff2390419315d79140b1ac88dd2441aa'
-        )
+        conn = self.connect()
+        
         cur = conn.cursor()
         cur.execute(query)
         conn.commit()
